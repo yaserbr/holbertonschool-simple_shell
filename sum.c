90afcb7e3679a2c1de93835426641a4bf6868081
yaser8541 <443017396@sm.imamu.edu.sa>
2026-08-23 20:30:45 +0300

    code added

---
+++ sum.c
@@ -0,0 +1,11 @@
+/**
+ * sum - calculates the sum of two integers
+ * @a: the first integer
+ * @b: the second integer
+ *
+ * Return: the sum of a and b
+ */
+int sum(int a, int b)
+{
+	return (a + b);
+}
